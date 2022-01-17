e3ed7b09da18b082ab0700f6ba9d2bd5e55281a4
Ananya0909 <ananyasgh941@gmail.com>
2022-01-17 06:55:58 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# friendlymarket-codelab-5kubnf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/friendlymarket-codelab-5kubnf)
---
+++ src/firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAmeITV3Z6V8aDFkBIgIXSsdOC8-QMftgk",
+  authDomain: "friendlymarket-8cb00.firebaseapp.com",
+  databaseURL: "https://friendlymarket-8cb00-default-rtdb.firebaseio.com",
+  projectId: "friendlymarket-8cb00",
+  storageBucket: "friendlymarket-8cb00.appspot.com",
+  messagingSenderId: "30560514204",
+  appId: "1:30560514204:web:bc8fedcb8d95adcf78fd59",
+  measurementId: "G-JTZ6BKG8FH"
+};
+
+export default firebaseConfig;
